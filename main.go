7eef1c25ab916fdcd6702e40d6a6dccb96a32ad9
Badara <badou017@gmail.com>
2024-12-25 22:13:44 +0000

    Setting up Bolt DB & Add Bucket

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/Badara-Senpai/cli_task_manager/task/cmd"
+import (
+	"fmt"
+	"github.com/Badara-Senpai/cli_task_manager/task/cmd"
+	"github.com/Badara-Senpai/cli_task_manager/task/db"
+	"github.com/mitchellh/go-homedir"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+
+	dbPath := filepath.Join(home, "tasks.db")
+
+	detect(db.Init(dbPath))
+	detect(cmd.RootCmd.Execute())
+}
+
+func detect(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
