b7cc85f8360ae877c33935bd9ca6dae0becc5af0
Badara <badou017@gmail.com>
2024-12-25 18:11:54 +0000

    Setting up the CLI Shell

---
+++ cmd/add.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"strings"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Adds a task to your task list",
+	Run: func(cmd *cobra.Command, args []string) {
+		task := strings.Join(args, " ")
+
+		for i, arg := range args {
+			fmt.Println(i, ":", arg)
+		}
+
+		fmt.Printf("Added \"%s\" to your task list", task)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(addCmd)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Badara-Senpai/cli_task_manager/task/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+}
