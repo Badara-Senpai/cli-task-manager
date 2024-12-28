689ea3da477567dea95a6edcaa03d2d9f4ce5bbf
Badara <badou017@gmail.com>
2024-12-28 18:44:31 +0000

    Performing Task Operations to Bolt

--- db/db.go
+++ db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"github.com/boltdb/bolt"
 	"time"
 )
@@ -26,4 +27,61 @@ func Init(dbPath string) error {
 
 		return err
 	})
+}
+
+func AllTask() ([]Task, error) {
+	var tasks []Task
+
+	err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(taskBucket)
+		c := bucket.Cursor()
+
+		for k, v := c.First(); k != nil; c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(taskBucket)
+		id64, _ := bucket.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return bucket.Put(key, []byte(task))
+	})
+
+	if err != nil {
+		return -1, err
+	}
+
+	return id, nil
+}
+
+func DeleteTask(key int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(taskBucket)
+		return bucket.Delete(itob(key))
+	})
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
 }
